9324c41b713fec9f4f2b04ad5134ba95d460c082
Falkenberg <nfalkenb@fredhutch.org>
2024-07-18 15:03:20 -0700

    Add CRC metrics to covariate data

--- read_exp_cov_files.R
+++ read_exp_cov_files.R
@@ -27,6 +27,9 @@ data_cnty_move <- read.csv("countyvars_moving_final.csv")
 # CDC Places county data file
 data_cnty_places <- read.csv("PLACES_FINAL.csv")
 
+# SEER CRC County Metrics
+data_cnty_crc <- read.csv("crc_county_metrics.csv")
+
 #### Join data ####
 ### Time varying data (per county per year)
 ## NO2 and Ozone
@@ -48,6 +51,30 @@ data_cnty_yr <- left_join(data_cnty_yr, data_cnty_tv,
 
 ### Time constant data (per county only)
 data_cnty_only <- full_join(data_cnty_move, data_cnty_places, by = "countyfips")
+
+## Preparing CRC metrics
+names(data_cnty_crc) <- c("CountyName", 
+                          "pct_CRC_2004-2007",
+                          "pct_Endo_2004-2007",
+                          "pct_FOBT_2004-2007",
+                          "pct_CRC_2008-2010",
+                          "pct_Endo_2008-2010",
+                          "pct_FOBT_2008-2010")
+
+# Extracting FIPS and converting metrics columns to numbers
+data_cnty_crc <- data_cnty_crc %>% 
+  filter(grepl("[0-9]+", CountyName)) %>% 
+  mutate(countyfips = regmatches(CountyName,
+                                  regexpr("[0-9]+", CountyName)),
+         countyfips = as.numeric(countyfips),
+         `pct_CRC_2004-2007` = as.numeric(`pct_CRC_2004-2007`)/100,
+         `pct_Endo_2004-2007` = as.numeric(`pct_Endo_2004-2007`)/100,
+         `pct_FOBT_2004-2007` = as.numeric(`pct_FOBT_2004-2007`)/100,
+         `pct_CRC_2008-2010` = as.numeric(`pct_CRC_2008-2010`)/100,
+         `pct_Endo_2008-2010` = as.numeric(`pct_Endo_2008-2010`)/100,
+         `pct_FOBT_2008-2010` = as.numeric(`pct_FOBT_2008-2010`)/100)
+
+data_cnty_only <- left_join(data_cnty_only, data_cnty_crc, by = "countyfips")
 
 ### Combine everything
 data_merged <- left_join(data_cnty_yr, data_cnty_only, by = "countyfips", relationship = "many-to-one")

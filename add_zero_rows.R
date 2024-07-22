d58d69b3702012e56b7ef1309764af0e93662898
Falkenberg <nfalkenb@fredhutch.org>
2024-07-22 15:10:29 -0700

    Script for adding missing zero count rows to incidence tables

---
+++ add_zero_rows.R
@@ -0,0 +1,63 @@
+source("read_freq_tables.R") # to read county codes
+
+#data_inc <- fread("data/incidence_table-LEFT.csv")
+
+make_grid <- function(rg){
+  # keys for county fips
+  county_codes_path <- paste("~/SEER Files/county_codes_", rg, ".txt", sep = "")
+  county_codes <- read_county_codes(county_codes_path)
+  
+  # table with every combination of variable values
+  grd <- expand.grid(county = county_codes$code,
+                     year_rx = 0:21,
+                     age_group = 0:3,
+                     sex = 0:1,
+                     marital_status = 0:2,
+                     race = 0:5,
+                     stage = c("distant", "insitu", "localized", "regional"))
+  
+  grd <- as.data.table(grd)
+  
+  # join county_fips
+  setkey(grd, county)
+  setkey(county_codes, code)
+  grd <- grd[county_codes,]
+  
+  grd <- grd %>% 
+    mutate(county_fips = as.numeric(county_fips))
+  
+  return(grd)
+}
+
+# grids <- lapply(c("SWMW", "Pac"), make_grid)
+# grids <- rbindlist(grids)
+# grids$count <- 0 # add count column
+# grids$site <- "LEFT" # add site column
+# 
+# ## add the missing zero rows to incidence data
+# key_cols <- setdiff(names(grids), c("count", "county_name"))
+# 
+# # perform anti-join
+# zero_rows <- grids[!data_inc, on = key_cols]
+# 
+# # add zero rows in
+# data_inc_full <- rbind(data_inc, zero_rows)
+
+
+add_zero_rows <- function(data_inc, site, reg_list){
+  grids <- lapply(reg_list, make_grid)
+  grids <- rbindlist(grids)
+  grids$count <- 0 # add count column
+  grids$site <- site # add site column
+  
+  ## add the missing zero rows to incidence data
+  key_cols <- setdiff(names(grids), c("count", "county_name"))
+  
+  # perform anti-join
+  zero_rows <- grids[!data_inc, on = key_cols]
+  
+  # add zero rows in
+  data_inc_full <- rbind(data_inc, zero_rows)
+  
+  return(data_inc_full)
+}
